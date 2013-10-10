173691794af272c3715ae5693adcf6ca0986b40f
Marcos Sainz <marcos.sainz+marcos@glassdoor.com>
2013-10-09 20:47:09 -0700

    Wrote more and cleaned up client tests

--- test/test_client.jl
+++ test/test_client.jl
@@ -9,8 +9,19 @@ client = redis()
 @test set(client, "foo", 4) == true
 @test set(client, "foo", "bananas"; nx=true) == false
 @test set(client, "foo", 4.53; xx=true) == true
-
 @test get(client, "foo") == "4.53"
+
+# key expiration via px (in milliseconds)
+@test set(client, "foo", 4; px=500) == true
+@test get(client, "foo") == "4"
+sleep(0.510)
+@test get(client, "foo") == nothing
+
+# key expiration via ex (in seconds)
+@test set(client, "foo", 4; ex=1) == true
+@test get(client, "foo") == "4"
+sleep(1.1)
+@test get(client, "foo") == nothing
 
 @test bgsave(client) == true
 
@@ -20,8 +31,12 @@ client = redis()
 
 @test dbsize(client) == 0
 
-@show info(client)
-@show info(client, "clients")
+dict = info(client)
+@test typeof( dict ) == Dict{Any, Any}
+@test length(dict) > 0
+dict = info(client, "clients")
+@test typeof( dict ) == Dict{Any, Any}
+@test length(dict) > 0
 
 @test ping(client) == true
 
@@ -29,7 +44,7 @@ client = redis()
 
 @test bgrewriteaof(client) == true
 
-@show time(client)
+@test typeof( time(client) ) == (Int, Int)
 
 @test append(client, "foo", "123") == length("123")
 @test append(client, "foo", "+45") == length("123+45")
